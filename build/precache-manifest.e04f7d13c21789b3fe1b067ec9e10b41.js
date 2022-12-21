6c894619333ef8ec015da269a5a28c5489f64961
Joe <brunery@gmail.com>
2022-12-20 21:35:12 -0300

    Generated build directory

---
+++ build/precache-manifest.e04f7d13c21789b3fe1b067ec9e10b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10a81daeec347970b2194d3fa8d2a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a747801a2196c4a97377",
+    "url": "/static/css/main.0626acd1.chunk.css"
+  },
+  {
+    "revision": "6aea775c8714fe6f0b1c",
+    "url": "/static/js/2.52f37571.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.52f37571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a747801a2196c4a97377",
+    "url": "/static/js/main.665fcfc7.chunk.js"
+  },
+  {
+    "revision": "a0604970fbe3dfb00076",
+    "url": "/static/js/runtime-main.d1798aaa.js"
+  }
+]);
